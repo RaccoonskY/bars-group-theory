6127e5916a3ad8e01c69e5c82596a4b110e4e178
RaccoonskY <viktoryablonskas@yandex.ru>
2023-09-01 14:38:15 +0300

    ISP and LCP examples added

---
+++ solid-principles-practice/ISP/ISPGoodExample.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solid_principles_practice.ISP
+{
+    internal class ISPGoodExample
+    {
+        public interface IOperationAdd
+        {
+            bool AddDetailsEmployee();
+        }
+
+        public interface IOperationGet
+        {
+            bool ShowDetailsEmployee(int id);
+        }
+    }
+}
