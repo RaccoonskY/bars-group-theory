caea82908dde19d0f718789f2178af7b5809b2f3
RaccoonskY <viktoryablonskas@yandex.ru>
2023-09-01 16:21:33 +0300

    IS principle added

--- solid-principles-practice/ISP/ISPGoodExample.cs
+++ solid-principles-practice/ISP/ISPGoodExample.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace solid_principles_practice.ISP
 {
+    /// <summary>
+    /// Вместо единого интерфейса мы разделяем потенциальные зависимости на два интерфейса
+    /// Если классу не требуется для реализации ShowDetailsEmployee, он может реализовать необходимый интерфейс
+    /// </summary>
     internal class ISPGoodExample
     {
         public interface IOperationAdd
